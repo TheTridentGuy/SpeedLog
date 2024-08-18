2dc085a65c0e35ce9c25a20951541ae407576fbb
aidenbohlander <aiden.bohlander@gmail.com>
2024-08-17 19:42:32 -0700

    finish basic interactivity

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import json
 
 
 app = Flask(__name__)
@@ -7,6 +8,20 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/log")
+def log():
+    data = request.values.get("log")
+    if data:
+        try:
+            data = json.loads(data)
+        except json.JSONDecodeError:
+            return "invalid json"
+        print(data)
+        return "success"
+    else:
+        return "missing/invalid data"
 
 
 if __name__ == '__main__':
