d2e2f7498d629d916421ecc58f7bf39a2fb1fe9b
aidenbohlander <aiden.bohlander@gmail.com>
2024-08-17 23:18:52 -0700

    huh whaddid i do

--- server.py
+++ server.py
@@ -1,8 +1,24 @@
 from flask import Flask, render_template, request
 import json
+import pathlib
 
 
 app = Flask(__name__)
+
+
+with open("config.json", "r") as cfg:
+    cfg = json.loads(cfg.read())
+    save_file = cfg.get("save_file")
+save_data = []
+if pathlib.Path(save_file).exists():
+    with open(save_file, "r") as f:
+        save_data = json.loads(f.read())
+
+
+def save_to_file():
+    with open(save_file, "w") as f:
+        f.write(json.dumps(save_data))
+
 
 
 @app.route("/")
@@ -11,7 +27,7 @@ def index():
 
 
 @app.route("/about")
-def index():
+def about():
     return render_template("about.html")
 
 
@@ -24,6 +40,8 @@ def log():
         except json.JSONDecodeError:
             return "invalid json"
         print(data)
+        save_data.append(data)
+        save_to_file()
         return "success"
     else:
         return "missing/invalid data"
